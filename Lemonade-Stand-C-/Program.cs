c72fa30272922fe241b113d2de0068ebd2146e7a
IseTrade <IseTrade@gmail.com>
2018-09-26 14:38:47 -0500

    Added methods for generating random daily and weekly weather conditions. Need to work differentiating betw. daily actual weather and daily forecasted weather.

--- Lemonade-Stand-C-/Program.cs
+++ Lemonade-Stand-C-/Program.cs
@@ -8,8 +8,18 @@ namespace Lemonade_Stand_C_
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            Weather weather = new Weather();
+
+            weather.DayForecast(); //tested ok for generating random daily weather.
+            Console.ReadLine();
+
+
+
+
         }
+        
     }
 }
--- Lemonade-Stand-C-/Weather.cs
+++ Lemonade-Stand-C-/Weather.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace Lemonade_Stand_C_
 {
-    class Weather
+    public class Weather
     {
-    }
+        public int dayScore;
+        public Weather()
+        {
+            //default constructor
+        }
+
+
+        public List<string> temperatureList = new List<string>()
+        {
+            "Cold",
+            "Warm",
+            "Hot"
+        };
+
+        public List<string> conditionList = new List<string>()
+        {
+            "Rainy",
+            "Overcast",
+            "Sunny"
+        };
+        
+
+        //randomize temperature list
+        public int GetRandomTemp()
+        {
+            Random rnd = new Random();
+            int temperatureIndex = rnd.Next(0, 3);
+            return temperatureIndex;
+        }
+
+        //randomize condition list
+        public int GetRandomCondition()
+        {
+            Random rnd = new Random();
+            int conditionIndex = rnd.Next(0, 3);
+            return conditionIndex;
+        }
+
+        public void DayForecast()
+        {
+            Console.WriteLine("The forecast for today is {0} and {1}.", temperatureList[GetRandomTemp()], conditionList[GetRandomCondition()]);
+        }
+
+        public void WeekForecast()
+        {
+            // add codes
+        }
+
+        public int DayScore() //score 1-4, with 4 being the best weather and temperature
+        {
+            return dayScore = GetRandomCondition() + GetRandomTemp();
+        }
+
+
+        }
 }
